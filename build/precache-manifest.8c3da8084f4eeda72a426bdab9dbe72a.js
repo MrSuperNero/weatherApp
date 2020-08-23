3ba17a5aebfb0ff9b73953e450a5d391d0fbcdba
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-08-23 17:29:27 -0400

    built website

---
+++ build/precache-manifest.8c3da8084f4eeda72a426bdab9dbe72a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c565ac2c2c46333ddec806bc4730208e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96161c0544a0b7ac7156",
+    "url": "/static/css/main.57f7150d.chunk.css"
+  },
+  {
+    "revision": "72cbf997e3aa8f24ab8d",
+    "url": "/static/js/2.270aa8af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.270aa8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96161c0544a0b7ac7156",
+    "url": "/static/js/main.ab2c1a77.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "066ed432d9012da18afab810f6574686",
+    "url": "/static/media/cloudy.066ed432.png"
+  },
+  {
+    "revision": "085fc2e7e8c8b15b05d23702fb1d6785",
+    "url": "/static/media/snow.085fc2e7.png"
+  },
+  {
+    "revision": "d74aa6c3bee85df0c1af227539fedf75",
+    "url": "/static/media/sunny.d74aa6c3.png"
+  }
+]);
